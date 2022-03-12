8c6c898b5bcb7a5354e0e4e3a1ac294a9210e4e6
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2022-03-12 14:29:01 +0100

    Update IDs to support loading

--- loaders/json_to_csv.py
+++ loaders/json_to_csv.py
@@ -16,20 +16,33 @@ stages = [
     ],
 ]
 
+uri_to_id = {}
+id_number = 1
 stage_number = 1
 for stage in stages:
     for file_item in stage:
+        file_type = file_item["type"]
         with open("../data/%s" % (file_item["filename"])) as json_file:
             print(file_item["filename"])
             data = json.load(json_file)
             for k, v in data.items():
-                csv_filename = "../data/stage_%d_%s_%s.csv" % (stage_number, k.lower(), file_item["type"])
+                csv_filename = "../data/stage_%d_%s_%s.csv" % (stage_number, k.lower(), file_type)
                 with open(csv_filename, mode='w') as csv_file:
-                    #fieldnames = ["label"] + list(v[0].keys())
-                    fieldnames = list(v[0].keys())
+                    if file_type == "nodes":
+                        fieldnames = [":ID"] + list(v[0].keys())
+                    else:
+                        fieldnames = [ ":START_ID", ":END_ID" ]
                     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
                     writer.writeheader()
                     for row in v:
-                        #row["label"] = k
-                        writer.writerow(row)
+                        if file_type == "nodes":
+                            row[":ID"] = id_number
+                            uri_to_id[row["uri"]] = id_number
+                            print("%s = %s" % (row["uri"], id_number))
+                            id_number += 1
+                            writer.writerow(row)
+                        else:
+                            new_row = { ":START_ID": uri_to_id[row["from"]], ":END_ID": uri_to_id[row["to"]] }
+                            writer.writerow(new_row)
+
     stage_number += 1
