24e33c378f040a4afa762b646765ec73d0f85cc3
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2022-03-18 14:09:25 +0100

    New load files

--- loaders/canonical_model.py
+++ loaders/canonical_model.py
@@ -10,17 +10,18 @@ nodes = { "CANONICAL_MODEL": [], "CANONICAL_NODE": [], "CANONICAL_DATA_TYPE": []
 relationships = { "HAS_SUB_MODEL": [], "CONSISTS_OF": [], "HAS_DATA_TYPE": [], "FROM_SOURCE": []}
 repeat = {}
 
-def process_nodes(node_set, parent_uri, rel_type):
+def process_nodes(node_set, parent_uri, rel_type, link_to_parent=True):
     for node in node_set:
         print("Node:", node[":name"])
         uri_name = format_name(node[":name"])
+        node_uri = "%s/%s" % (parent_uri, uri_name)
         if not node[":name"] in repeat:
-            node_uri = "%s/%s" % (parent_uri, uri_name)
             nodes["CANONICAL_NODE"].append({ "name": node[":name"], "uri": node_uri })
             repeat[node[":name"]] = node_uri
         else:
             node_uri = repeat[node[":name"]]
-        relationships[rel_type].append({"from": parent_uri, "to": node_uri})
+        if link_to_parent:
+            relationships[rel_type].append({"from": parent_uri, "to": node_uri})
         if ":nodes" in node:
             process_nodes(node[":nodes"], node_uri, "CONSISTS_OF")
         else:
@@ -33,7 +34,7 @@ def process_nodes(node_set, parent_uri, rel_type):
                         "uri": item_uri
                     }
                     nodes["CANONICAL_DATA_TYPE"].append(record)
-                    relationships["HAS_DATA_TYPE"].append({"from": parent_uri, "to": item_uri})
+                    relationships["HAS_DATA_TYPE"].append({"from": node_uri, "to": item_uri})
 
 with open("../data/canonical/canonical_model.yaml") as file:
     model = yaml.load(file, Loader=yaml.FullLoader)
@@ -41,6 +42,7 @@ with open("../data/canonical/canonical_model.yaml") as file:
     # Source
     source_uri = "http://id.d4k.dk/dataset/source/canonical"
     base_uri = "http://id.d4k.dk/dataset/canonical"
+    common_uri = "%s/common" % (base_uri)
     record = {
         "uri": source_uri, 
         "description": "Designed for this project."
@@ -50,6 +52,7 @@ with open("../data/canonical/canonical_model.yaml") as file:
     nodes["CANONICAL_MODEL"].append({ "name": model[":root"][":name"], "uri": base_uri })
     relationships["FROM_SOURCE"].append({"from": base_uri, "to": source_uri})
     parent_uri = base_uri
+    process_nodes(model[":common"][":nodes"], common_uri, "CONSISTS_OF", False)
     process_nodes(model[":root"][":nodes"], base_uri, "HAS_SUB_MODEL")
 
 with open("../data/canonical/canonical_model_nodes.json", 'w') as outfile:
